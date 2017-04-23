5aedd2c3508c2dcbbdc7c91a4f47213dba49c0fa
Dooho Yi <pilotedeguerre@gmail.com>
2017-04-23 13:09:12 +0900

    player

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 /* bug: user touches too fast. then, sth. breaks! --> utilize ready function (below)? */
 
+//debug
+var g_evt;
+
 $( document ).ready(function() {
 
     // connect server
@@ -14,41 +17,28 @@ $( document ).ready(function() {
 	oscPort.on("message", function (oscMsg) {
 	    if (oscMsg.address == "/rollcnt") $("#rollcnt").text(oscMsg.args[0].value); // dim led?
 	});
-
-	// var tid = setInterval(function () {
-	    oscPort.send({
-		address: "/occupy",
-		args: [{ type: "f", value: 10 }]
-	    });
-	// }, 500);
     });
 
-    // entry page
-    
-    $("#entry").click(function() {
-	$(this).fadeOut(500);
-	$("#page1").fadeIn(500);
-	//alternative way 1
-	/* $(this).fadeOut(500, function() {
-	   $("#page1").fadeIn(500);
-	   * });*/
-	//alternative way 2
-	/* $(this).fadeToggle("slow", "linear");
-	 * $("#page1").fadeToggle("slow", "linear");*/
-    });
+    // reserve seat page
 
-    // page 1
-    
-    $("#form1").submit(function(event) {
+    $("#selseat").submit(function(event) {
 	event.preventDefault(); // to stop refreshing.. (actual submit action)
-	
-	// //Loading..
-	//$("#page1-left").removeClass("bg-yellow blue").addClass("bg-purple white-70");
-	//$("#page1-msg").removeClass("f6 f3-ns").addClass("f4 f2-ns");
-	//$("#page1-msg").text("다운로드 중..");
 
+	// //check data
+	// g_evt = event;
+	// // if (event.
+
+	// // var tid = setInterval(function () {
+	//     oscPort.send({
+	// 	address: "/occupy",
+	// 	args: [{ type: "f", value: 10 }]
+	//     });
+	// // }, 500);
+
+	// //if ok..
+	
 	//go to Loading page...
-	$("#page1").hide();
+	$("#resvseat").hide();
 	$("#loading").show();
 
 	//load data
@@ -63,10 +53,50 @@ $( document ).ready(function() {
 	player1 = new Tone.Player({ "url" : "./audio/01.mp3" }).toMaster();
 	Tone.Buffer.on("load", function(){
 	    $("#loading").fadeOut(500);
-	    $("#page2").fadeIn(500);
-	}.bind(this)); //-->resolve scoping issues.. : https://www.smashingmagazine.com/2014/01/understanding-javascript-function-prototype-bind/
+	    $("#play1").fadeIn(500);
+	}.bind(this));
+	//-->resolve scoping issues.. : https://www.smashingmagazine.com/2014/01/understanding-javascript-function-prototype-bind/
     }
 
-    // page 2
+    // player session #1
+
+    //initial state
+    var playing1 = 0;
+    $('#playbtn #path4493').show();
+    $('#playbtn #rect8475').hide();
+    $('#playbtn #rect8475-3').hide();
+    
+    $('#playbtn').click(function() {
+	if (playing1 == 0) {
+	    playing1 = 1;
+	    playstart1();
+	} else if (playing1 == 1) {
+	    playing1 = 0;
+	    playstop1();
+	}
+    });
+
+    function playstart1() {
+	player1.start();
+	// btn shape change.
+	$('#playbtn #path4493').hide();
+	$('#playbtn #rect8475').show();
+	$('#playbtn #rect8475-3').show();
+
+	var p1id = setInterval(function() {
+	    if(player1.state == "stopped") {
+		playstop1();
+		clearInterval(p1id);
+	    }
+	}, 500);
+    }
+
+    function playstop1() {
+	player1.stop();
+	// btn shape change.
+	$('#playbtn #path4493').show();
+	$('#playbtn #rect8475').hide();
+	$('#playbtn #rect8475-3').hide();
+    }
 
 });
